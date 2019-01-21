d50ed84ed108ce0c5d9c7e9fc6d92f58d5229b9d
Your Name <you@example.com>
2019-01-21 14:42:31 -0700

    Initial Commit

---
+++ Final project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final project
+//
+//  Created by Noah Martineau Raymundo on 2019-01-21.
+//  Copyright © 2019 Phidget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
